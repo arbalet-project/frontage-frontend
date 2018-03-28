387cbd7ccd87430b4e580b2ade7c6f1d6cde92ff
Netsab612 <bastien.meunier@yahoo.fr>
2018-03-28 21:55:28 +0200

    Stabilize GUI to avoid bug and little problems

--- frontage-frontend-app/src/pages/home/home.ts
+++ frontage-frontend-app/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
 import { LoginPage } from '../login/login';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'page-home',
@@ -11,7 +12,8 @@ export class HomePage {
   
   slideData = [{ image: "assets/img/SweepRand.jpeg" },{ image: "assets/img/SweepAsync.jpeg" },{ image: "assets/img/home_screen.jpg" }];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loca: Location) {
   }
   
   goToLoginPage(){
