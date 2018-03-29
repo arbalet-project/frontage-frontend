f0ee24e4bce858086bcab60221d8a16feedbadbd
tvalli <valli.thomas@gmail.com>
2018-03-29 19:19:18 +0200

    Add the admin part on the snake options page

--- frontage-frontend-app/src/pages/snake-options/snake-options.ts
+++ frontage-frontend-app/src/pages/snake-options/snake-options.ts
@@ -1,3 +1,4 @@
+import { LocalStorageProvider } from './../../providers/local-storage/local-storage';
 import { SnakeJoystickPage } from './../snake-joystick/snake-joystick';
 import { WaitingPage } from './../waiting/waiting';
 import { DataFAppsProvider } from './../../providers/data-f-apps/data-f-apps';
@@ -14,9 +15,11 @@ export class SnakeOptionsPage {
 
   fAppOptions: FormGroup;
   fAppPosition: number;
+  isAdmin: boolean = false;
 
-  constructor(public navCtrl: NavController, public dataFAppsProvider: DataFAppsProvider, public formBuilder: FormBuilder) {
-    console.log("snake option")
+  constructor(public navCtrl: NavController, public dataFAppsProvider: DataFAppsProvider, public formBuilder: FormBuilder, public localStorageProvider : LocalStorageProvider) {
+    //Check if the connected user is admin
+    this.isAdmin = this.localStorageProvider.isAdmin();
 
     this.fAppOptions = formBuilder.group({
       fAppColor: ""
@@ -30,7 +33,6 @@ export class SnakeOptionsPage {
       params: {
       }
     }
-
     
     this.dataFAppsProvider.launchFApp(options)
       .subscribe(response => this.goToNextPage(response));
@@ -41,4 +43,12 @@ export class SnakeOptionsPage {
     this.navCtrl.push(WaitingPage, {info:response, joystick:SnakeJoystickPage});
   }
 
+  forceFapp() {
+    let options: FAppOptions = {
+      name: "Snake",
+      params: {}
+    };
+    this.dataFAppsProvider.launchForcedFApp(options)
+      .subscribe(response => response);
+  };
 }
