1fd4f50124e5c218f377572c5a737c0eca7508e8
Netsab612 <bastien.meunier@yahoo.fr>
2018-05-09 17:34:02 +0200

    Improve transition to and from error page so the stack of pages is correctly set after entering it without bliking

--- frontage-frontend-app/src/pages/error/error.ts
+++ frontage-frontend-app/src/pages/error/error.ts
@@ -10,22 +10,38 @@ export class ErrorPage {
 
   errorMessage: String;
   isDisplayed: boolean = false;
+  isPopping: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    console.log("entered")
     this.errorMessage = this.navParams.get("errorMessage");
+    
   }
 
   displayError() {
+    console.log("toggle display")
     this.isDisplayed = !this.isDisplayed;
   }
 
   quitPage() {
+    console.log("Quit page")
     this.navCtrl.push(LoginPage).then(() => this.navCtrl.remove(this.navCtrl.getPrevious().index))
   }
-  
-  ionViewDidEnter(){
-    while((this.navCtrl.getPrevious() != this.navCtrl.getByIndex(0)) && (this.navCtrl.getActive() !=  this.navCtrl.getByIndex(0))) {
-      this.navCtrl.remove(this.navCtrl.getPrevious().index);
+
+  ionViewDidEnter() {
+    if(!this.isPopping) {
+      this.isPopping = true
+      this.popPageBelow()
+    }
+  }
+
+  popPageBelow() {
+    
+    if ((this.navCtrl.getPrevious().id != this.navCtrl.getByIndex(0).id) && (this.navCtrl.getActive().id != this.navCtrl.getByIndex(0).id)) {
+      let self=this;
+      this.navCtrl.remove(this.navCtrl.getPrevious().index).then(() => self.popPageBelow())
+    }else {
+      this.isPopping = false
     }
   }
 }
