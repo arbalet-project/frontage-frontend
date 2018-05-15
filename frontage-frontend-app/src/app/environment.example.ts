f5a69240b81fbbd128305f776ec2e87e8fc57ff1
Yoan Mollard <ymollard@users.noreply.github.com>
2018-05-15 12:23:46 +0200

    Cleanup environment + update sentry dev env

--- frontage-frontend-app/src/app/environment.example.ts
+++ frontage-frontend-app/src/app/environment.example.ts
@@ -3,7 +3,5 @@ export const environmentExample = {
     snapBaseUrl: "http://192.168.1.18:33450",
     webSocketAdress: "ws://192.168.1.18:8124",
     protocol_version: 1,
-    sentryUri: "http://2cdae4b5898d4475bafd33bd4a5f71ed@192.168.1.23:9000/3",
-    signedPublicKey : "AAAAB3NzaC1yc2EAAAADAQABAAABAQDvDyGSzaU1z0dMkqVrx5UD9Ss+HVOheGgcb1kYHjt8/P9sMi09EQLov/7IN/1wMfXbfqDsHv6+Vqx3Vf/u0H40fQwKTphFL/4Fx+ysBr5YEB9bkzipSLbNBbpsjkZl5Ba/vfajoJdnvjnTj3Qm+z+pCgQeO6PDTh5m6Z0xuCc8NQQ3cyWIKvBWGbh01h+h7ZxrfETk7be5iIn+uX8ugnXkRFlPUNE70RxcvQMn3IzJCYCI2xbrvmnmm/bSl5W4e6CMfyRLHM7KDf4BYng/uAo6aYx93YKCnl6WKUkk+YFDHdh4TykQoGPUK4SGyk7QV2V4wLZY3xiUF0dOYdYjehTp",
-    snapPort: "33450"
+    sentryUri: "http://04bc4e63f4964b94a230aa52e048c802@192.168.0.42:9000/3"
 }
