51b4c719e1982a37eeef6cd5daf758c698fb4bb4
Yoan Mollard <ymollard@users.noreply.github.com>
2018-05-23 00:13:45 +0200

    Updated environment

--- frontage-frontend-app/src/app/environment.example.ts
+++ frontage-frontend-app/src/app/environment.example.ts
@@ -1,7 +1,7 @@
 export const environmentExample = {
-    backEndBaseUrl: "http://192.168.1.18:8123",
+    backEndBaseUrl: "http://192.168.1.18:33405",
     snapBaseUrl: "http://192.168.1.18:33450",
-    webSocketAdress: "ws://192.168.1.18:8124",
+    webSocketAdress: "ws://192.168.1.18:33406",
     protocol_version: 1,
     sentryUri: "http://04bc4e63f4964b94a230aa52e048c802@192.168.0.42:9000/3"
 }
