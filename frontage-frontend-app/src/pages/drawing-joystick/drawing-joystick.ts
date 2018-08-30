e91a4746d44a2c782be610fddbaa76401698c3b2
Netsab612 <bastien.meunier@yahoo.fr>
2018-08-30 23:30:01 +0200

    improve drawing

--- frontage-frontend-app/src/pages/drawing-joystick/drawing-joystick.ts
+++ frontage-frontend-app/src/pages/drawing-joystick/drawing-joystick.ts
@@ -1,3 +1,5 @@
+import { DataFAppsProvider } from './../../providers/data-f-apps/data-f-apps';
+import { WebsocketMessageHandlerProvider } from './../../providers/websocket-message-handler/websocket-message-handler';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
@@ -7,11 +9,41 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class DrawingJoystickPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  parametersList: string;
+  selectedParameter: string[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public fAppProvider: DataFAppsProvider,
+    public websocketMessageHandler: WebsocketMessageHandlerProvider) {
+
+    let joystickParams = navParams.get('joystickParams');
+
+    this.parametersList = joystickParams.parametersList;
+    this.selectedParameter = joystickParams.selectedParameter;
+
+    websocketMessageHandler.initSocket(navCtrl);
+  }
+
+  sendColor(truc) {
+
+    console.log(JSON.stringify(truc))
+    // let colors = {red:255, blue:255, green:255}
+    // this.websocketMessageHandler.send(JSON.stringify(colors))
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DrawingJoystickPage');
+    console.log('ionViewDidLoad DrawingJoystickPage')
   }
 
+  stopFApp() {
+    this.navCtrl.pop();
+  }
+
+  ionViewDidLeave() {
+
+    if (!this.websocketMessageHandler.isExternalyClaused()) {
+      this.fAppProvider.stopApp();
+      this.websocketMessageHandler.closeSocket();
+    }
+    this.websocketMessageHandler.stopKeepAliveSender();
+  }
 }
