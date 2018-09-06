65a7a2d14e3cb3b35370f9c9671ceecdf92b08b8
Yoan Mollard <ymollard@users.noreply.github.com>
2018-09-06 23:14:23 +0200

    Silently ignore unknown fApps

--- frontage-frontend-app/src/pages/f-app-list/f-app-list.ts
+++ frontage-frontend-app/src/pages/f-app-list/f-app-list.ts
@@ -23,7 +23,7 @@ import { SnapJoystickPage } from '../snap-joystick/snap-joystick';
 })
 export class FAppListPage {
 
-  fAppList: FApp[];
+  fAppList: FApp[] = [];
   fAppPosition: number;
   isAdmin: boolean = false;
   isFacadeUp: boolean = false;
@@ -45,7 +45,16 @@ export class FAppListPage {
   ionViewWillEnter() {
     //Get the f-app list
     this.fAppsData.getList()
-      .subscribe(fAppList => this.fAppList = fAppList, err => console.log(err));
+      .subscribe(fAppList => {
+        fAppList.forEach(fApp => {
+          // if option page == undefined this app is unknown to the frontend, ignore it
+          if(this.establishNavigationPageName(fApp.name)) {
+            this.fAppList.push(fApp);
+          } else {
+            console.log("This app '" + fApp.name + "' is not know to the frontend, skipping");
+          }
+        });
+      }, err => console.log(err));
 
     this.authentication.isFacadeUp()
       .subscribe(res => {
@@ -57,7 +66,13 @@ export class FAppListPage {
 
   showOptions(fApp: FApp) {
     if (!this.isForced) {
-      this.navCtrl.push(this.establishNavigationPageName(fApp.name), { selectedFapp: fApp });
+      let optionsPage = this.establishNavigationPageName(fApp.name);
+      if(optionsPage) {
+        this.navCtrl.push(this.establishNavigationPageName(fApp.name), { selectedFapp: fApp });
+      }
+      else {
+        console.log("This app '" + fApp.name + "' is not know to the frontend, skipping");
+      }
     } else if ('Snap' == this.currentApp) {
       this.navCtrl.push(SnapJoystickPage, { joystickParams: "" });
     }
@@ -120,7 +135,7 @@ export class FAppListPage {
         return DrawingOptionsPage;
       }
       default: {
-        return FlagsOptionsPage;
+        return undefined;
       }
     }
   }
