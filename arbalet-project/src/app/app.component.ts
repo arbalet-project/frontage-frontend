44492433aeec94cb730264a133f110bbb28f1ce8
Etienne Schmitz <hello@etienne-schmitz.com>
2020-07-06 15:44:39 +0200

    add capacitor keep screen on plugin

--- arbalet-project/src/app/app.component.ts
+++ arbalet-project/src/app/app.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { TranslateService } from '@ngx-translate/core';
-
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { TranslateService } from "@ngx-translate/core";
+import { Plugins } from "@capacitor/core";
+const { CapacitorKeepScreenOn } = Plugins;
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private translate: TranslateService,
+    private translate: TranslateService
   ) {
     this.initializeApp();
   }
@@ -26,6 +27,11 @@ export class AppComponent {
       this.splashScreen.hide();
     });
 
-    this.translate.setDefaultLang('fr');
+    this.translate.setDefaultLang("fr");
+
+    if (!this.platform.is('mobile')) {
+      // Enable plugin to keep screen on
+      CapacitorKeepScreenOn.enable();
+    }
   }
 }
