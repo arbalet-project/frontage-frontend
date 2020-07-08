7edfa8622003d4fe82d2597a1f322c75f313fd6b
Etienne Schmitz <hello@etienne-schmitz.com>
2020-07-08 11:27:29 +0200

    add flags radio button and translation

--- arbalet-project/src/app/core/f-app/models/f-app.ts
+++ arbalet-project/src/app/core/f-app/models/f-app.ts
@@ -3,7 +3,7 @@ export class FApp {
     name: string;
     activated: boolean;
     playable: boolean;
-    params_list: {};
+    params_list: any; // TODO : We can improve this type
     scheduled: boolean;
 }
 // tslint:enable:variable-name
