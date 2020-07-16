47ddc8bd07315399aa755e8738c6acb954aeb61c
Etienne Schmitz <hello@etienne-schmitz.com>
2020-07-16 19:19:31 +0200

    add options page to random flashing

--- arbalet-project/src/app/f-app/randomflashing/randomflashing.module.ts
+++ arbalet-project/src/app/f-app/randomflashing/randomflashing.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { RandomflashingPageRoutingModule } from './randomflashing-routing.module';
 
 import { RandomflashingPage } from './randomflashing.page';
+import { OptionsPage } from './options/options.page';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { RandomflashingPage } from './randomflashing.page';
     IonicModule,
     RandomflashingPageRoutingModule
   ],
-  declarations: [RandomflashingPage]
+  declarations: [RandomflashingPage, OptionsPage]
 })
 export class RandomflashingPageModule {}
