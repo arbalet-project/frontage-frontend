020558b76a30b5a259c25ab10ac9d94cbd61cd1b
Etienne Schmitz <hello@etienne-schmitz.com>
2020-09-09 11:19:50 +0200

    resolve tracking serve error

--- src/app/core/plugins/tracking.service.ts
+++ src/app/core/plugins/tracking.service.ts
@@ -1,27 +1,35 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TrakingService {
   baseUrl = `${environment.trackingUrl}`;
   baseTracker = `/matomo.php?rec=1&idsite=${environment.idTrackingSite}`;
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   public connection() {
-    this.http.post(this.baseUrl + this.baseTracker + '&action_name=Connection', '').subscribe();
+    this.http
+      .post(this.baseUrl + this.baseTracker + "&action_name=Connection", "")
+      .subscribe(null, (_) => {});
   }
 
   public selectEvent(name: string) {
-    this.http.post(this.baseUrl + this.baseTracker + '&e_c=' + name + '&e_a=Select', '').subscribe();
+    this.http
+      .post(
+        this.baseUrl + this.baseTracker + "&e_c=" + name + "&e_a=Select",
+        ""
+      )
+      .subscribe(null, (_) => {});
   }
 
   public playEvent(name: string) {
-    this.http.post(this.baseUrl + this.baseTracker + '&e_c=' + name + '&e_a=Play', '').subscribe();
+    this.http
+      .post(this.baseUrl + this.baseTracker + "&e_c=" + name + "&e_a=Play", "")
+      .subscribe(null, (_) => {});
   }
-
-
 }
